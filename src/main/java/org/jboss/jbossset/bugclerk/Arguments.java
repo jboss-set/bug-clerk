f1929038cc6ce6d59829c06b7aed7ba29eb21334
Romain PELISSE <belaran@gmail.com>
2015-01-22 16:14:50 +0100

    Rework Arguments class to turn BZ ids into a main arg and a list (arity)

--- src/main/java/org/jboss/jbossset/bugclerk/Arguments.java
+++ src/main/java/org/jboss/jbossset/bugclerk/Arguments.java
@@ -1,12 +1,19 @@
 package org.jboss.jbossset.bugclerk;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beust.jcommander.IVariableArity;
 import com.beust.jcommander.Parameter;
 
-public class Arguments {
+public class Arguments implements IVariableArity {
 
-	// TODO make a converter to check ID
-	@Parameter(names = { "-i", "--url-issue-id" }, description = "Issue ID", required = true)
-	private String issueId;
+	// TODO make a converter to turn directly into an URL
+	@Parameter(names = { "-u", "--url-prefix" }, description = "URL prefix (before the issue ID)", required = true)
+	private String urlPrefix;
+
+	@Parameter(description = "Issue IDs", variableArity = true)
+	private final List<String> ids = new ArrayList<String>();
 
 	@Parameter(names = { "-h", "--help" }, description = "print help text", required = false)
 	private boolean help = false;
@@ -30,20 +37,26 @@ public class Arguments {
 		this.help = help;
 	}
 
-	public String getIssueId() {
-		return issueId;
-	}
 
-	public void setIssueId(String issueId) {
-		this.issueId = issueId;
-	}
 
-	@Override
+	public String getUrlPrefix() {
+        return urlPrefix;
+    }
+
+    public void setUrlPrefix(String urlPrefix) {
+        this.urlPrefix = urlPrefix;
+    }
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    @Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + (help ? 1231 : 1237);
-		result = prime * result + ((issueId == null) ? 0 : issueId.hashCode());
+		result = prime * result + ((urlPrefix == null) ? 0 : urlPrefix.hashCode());
 		return result;
 	}
 
@@ -58,17 +71,28 @@ public class Arguments {
 		Arguments other = (Arguments) obj;
 		if (help != other.help)
 			return false;
-		if (issueId == null) {
-			if (other.issueId != null)
+		if (urlPrefix == null) {
+			if (other.urlPrefix != null)
 				return false;
-		} else if (!issueId.equals(other.issueId))
+		} else if (!urlPrefix.equals(other.urlPrefix))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Arguments [issueId=" + issueId + ", help=" + help + "]";
+		return "Arguments [issueId=" + urlPrefix + ", help=" + help + "]";
 	}
+
+    @Override
+    public int processVariableArity(String optionName, String[] options) {
+        if ( "-i".equals(optionName) || "--ids".equals(optionName) ) {
+            for ( String id : options ) {
+                this.ids.add(id);
+            }
+            return this.ids.size();
+        }
+        return 0;
+    }
 
 }
