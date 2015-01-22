15908e5f0fb0bf6ec3503a57f3932c18a5b49f22
Romain PELISSE <belaran@gmail.com>
2015-01-22 21:04:30 +0100

    Add commodity script to run bugclerk

---
+++ src/main/bash/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+readonly BUG_CLERK_HOME=${BUG_CLERK_HOME:-'.'}
+readonly JAR_NAME=${JAR_NAME:-'bugclerk'}
+readonly VERSION=${VERSION:-'0.1'}
+readonly BZ_SERVER_URL=${BZ_SERVER_URL:-'https://bugzilla.redhat.com/show_bug.cgi?id='}
+
+checkScriptDependency() {
+  local cmd=${1}
+
+  which ${cmd} > /dev/null
+  if [ "${?}" -ne 0 ]; then
+    echo "This scripts requires command '${cmd}' which is missing from PATH."
+    exit 1
+  fi
+}
+
+checkScriptDependency java
+
+if [ ! -e "${BUG_CLERK_HOME}" ]; then
+  echo "The BUG_CLERK_HOME '${BUG_CLERK_HOME}' provided does not exist."
+  exit 2
+fi
+
+java -jar "${BUG_CLERK_HOME}/${JAR_NAME}-${VERSION}.jar" -u "${BZ_SERVER_URL}" "${1}"
