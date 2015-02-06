8939961e2ae4a0bb0acfbf8722051bef08ee0600
Romain PELISSE <belaran@gmail.com>
2015-02-06 13:49:39 +0100

    Add unit test for the ReportEngine

---
+++ src/test/java/org/jboss/jbossset/bugclerk/ReportEngineTest.java
@@ -0,0 +1,22 @@
+package org.jboss.jbossset.bugclerk;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.jbossset.bugclerk.utils.CollectionUtils;
+import org.junit.Test;
+
+public class ReportEngineTest {
+
+    @Test
+    public void test() {
+        final String dummyUrl = "http://dummy.url/";
+        final int bugId = 1;
+        final String checkname = "checkname";
+
+        ReportEngine engine = new ReportEngine(dummyUrl);
+        String report = engine.createReport(CollectionUtils.indexedViolationsByBugId(MockUtils.mockViolationsListWithOneItem(bugId, checkname)));
+        assertTrue(report.contains(dummyUrl));
+        assertTrue(report.contains(checkname));
+    }
+
+}
