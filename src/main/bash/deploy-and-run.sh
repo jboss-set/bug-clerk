82459eb64efc6ccabdb2eee5d7661690813ff32a
Romain PELISSE <belaran@gmail.com>
2015-03-03 15:56:18 +0100

    fix in the deploy-and-run script

--- src/main/bash/deploy-and-run.sh
+++ src/main/bash/deploy-and-run.sh
@@ -4,7 +4,10 @@ set -e
 export BUGCLERK_VERSION=${BUGCLERK:-'0.4.1.Final'}
 export BUGCLERK_JAR=${BUGCLERK_JAR:-"bugclerk-${BUGCLERK_VERSION}.jar"}
 export BUGCLERK_RELEASE_URL=${BUGCLERK_RELEASE_URL:-"https://github.com/jboss-set/bug-clerk/releases/download/bugclerk-${BUGCLERK_VERSION}/${BUGCLERK_JAR}"}
-export BUGCLERK_SCRIPT=${BUGCLERK_SCRIPT:-'filter-based-run.sh'}
+
+readonly BUGCLERK_SCRIPT=${BUGCLERK_SCRIPT:-'filter-based-run.sh'}
+readonly BUGCLERK_PROPS_FILE=${BUGCLERK_PROPS_FILE:-'/home/rpelisse/bugclerk.properties'}
+
 
 echo -n "Download BugClerk release from ${BUGCLERK_RELEASE_URL}... "
 wget ${BUGCLERK_RELEASE_URL} -O "${BUGCLERK_JAR}"
@@ -15,6 +18,7 @@ export BUGCLERK_HOME=$(pwd)
 readonly UNZIP_DIR=$(mktemp -d)
 unzip "${BUGCLERK_JAR}" -d "${UNZIP_DIR}"
 cp --preserve "${UNZIP_DIR}/${BUGCLERK_SCRIPT}" .
+cp "${BUGCLERK_PROPS_FILE}" .
 
 export FILTER_URL='https://bugzilla.redhat.com/buglist.cgi?cmdtype=dorem&remaction=run&namedcmd=jboss-eap-6.4.z-superset&sharer_id=213224&ctype=csv'
 
