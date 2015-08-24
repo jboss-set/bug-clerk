684c2b9b763564b7d2fcbc532ccc34c29d70a897
Martin Stefanko <mstefank@dhcp-10-40-5-197.brq.redhat.com>
2015-08-24 13:15:16 +0200

    HTML report review

--- src/main/java/org/jboss/jbossset/bugclerk/Violation.java
+++ src/main/java/org/jboss/jbossset/bugclerk/Violation.java
@@ -32,10 +32,10 @@ public class Violation {
     private void constructorSanityCheck(Candidate candidate, String mssg) {
         if (candidate == null)
             throw new IllegalArgumentException("Can't instantiate " + this.getClass().getCanonicalName()
-                    + " withou a 'null' bug ref.");
+                    + " with a 'null' bug ref.");
         if (mssg == null || "".equals(mssg))
             throw new IllegalArgumentException("Can't instantiate " + this.getClass().getCanonicalName()
-                    + " withou a 'null' or empty message.");
+                    + " with a 'null' or empty message.");
     }
 
     public Violation(Candidate candidate, String checkName, String message) {
