5a7e23d3666f5ad34b49b00859a429d91aa851ee
Romain Pelisse <belaran@gmail.com>
2016-01-21 22:08:12 +0100

    Refactor: removes not unneeded classes

--- src/test/java/org/jboss/jbossset/bugclerk/ReportEngineTest.java
+++ src/test/java/org/jboss/jbossset/bugclerk/ReportEngineTest.java
@@ -2,9 +2,10 @@ package org.jboss.jbossset.bugclerk;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.stream.Collectors;
+
 import org.jboss.jbossset.bugclerk.reports.ReportEngine;
 import org.jboss.jbossset.bugclerk.reports.StringReportEngine;
-import org.jboss.jbossset.bugclerk.utils.CollectionUtils;
 import org.junit.Test;
 
 public class ReportEngineTest {
@@ -16,10 +17,9 @@ public class ReportEngineTest {
         final String checkname = "checkname";
 
         ReportEngine<String> engine = new StringReportEngine();
-        String report = engine.createReport(CollectionUtils.indexedViolationsByBugId(MockUtils.mockViolationsListWithOneItem(
-                bugId, checkname)));
+        String report = engine.createReport(MockUtils.mockViolationsListWithOneItem(bugId, checkname).parallelStream()
+                .collect(Collectors.groupingBy(v -> v.getCandidate().getBug())));
         assertTrue(report.contains(dummyUrl));
         assertTrue(report.contains(checkname));
     }
-
 }
