9f95bc5067ae2960a7bee1f9dc9d4cad80813d95
Romain Pelisse <belaran@gmail.com>
2016-04-11 13:51:54 +0200

    Update scripts to use JIRA by default

--- src/main/bash/filter-based-run.sh
+++ src/main/bash/filter-based-run.sh
@@ -2,14 +2,17 @@
 
 readonly TRACKER_USERNAME=${TRACKER_USERNAME}
 readonly TRACKER_PASSWORD=${TRACKER_PASSWORD}
-readonly TRACKER_TYPE=${TRACKER_TYPE:-'bugzilla'}
+readonly TRACKER_TYPE=${TRACKER_TYPE:-'jira'}
 
 readonly BUGCLERK_HOME=${BUGCLERK_HOME:-'.'}
 readonly BUGCLERK_VERSION=${BUGCLERK_VERSION:-${project.version}}
 readonly JAR_NAME=${JAR_NAME:-'bugclerk'}
 
 readonly BZ_SERVER_URL='https://bugzilla.redhat.com'
-readonly FILTER_URL=${FILTER_URL:-"${BZ_SERVER_URL}/buglist.cgi?cmdtype=dorem&list_id=3303150&namedcmd=jboss-eap-6.4.z-superset&remaction=run&sharer_id=213224&ctype=csv"}
+
+readonly EAP7_UNRESOLVED='12326686'
+readonly FILTER_URL_ENDPOINT='https://issues.jboss.org/rest/api/latest/filter'
+readonly FILTER_URL=${FILTER_URL:-"${FILTER_URL_ENDPOINT}/${EAP7_UNRESOLVED}"}
 
 usage() {
   echo "$(basename ${0})"
