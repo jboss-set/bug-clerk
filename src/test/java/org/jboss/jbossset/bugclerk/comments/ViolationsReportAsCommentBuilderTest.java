9a89f501c0fbbef29c005537145a7a6b186a3eb1
Romain Pelisse <belaran@gmail.com>
2016-04-13 17:03:34 +0200

    Add unit test for the ViolationsReportAsCommentBuilder logic

--- src/test/java/org/jboss/jbossset/bugclerk/comments/ViolationsReportAsCommentBuilderTest.java
+++ src/test/java/org/jboss/jbossset/bugclerk/comments/ViolationsReportAsCommentBuilderTest.java
@@ -3,36 +3,71 @@ package org.jboss.jbossset.bugclerk.comments;
 import org.jboss.jbossset.bugclerk.MockUtils;
 import org.jboss.jbossset.bugclerk.Violation;
 import org.jboss.jbossset.bugclerk.checks.AssignedButStillOnSET;
+import org.jboss.jbossset.bugclerk.checks.PostMissingPR;
 import org.jboss.jbossset.bugclerk.checks.PostMissingPmAck;
+import org.jboss.jbossset.bugclerk.utils.StringUtils;
 import org.jboss.set.aphrodite.domain.Comment;
 import org.jboss.set.aphrodite.domain.Issue;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Created by rpelisse on 12/04/16.
  */
 public class ViolationsReportAsCommentBuilderTest {
 
+    private ViolationsReportAsCommentBuilder builder;
+
+    @Before
+    public void buildTestInstance() {
+        builder = new ViolationsReportAsCommentBuilder();
+    }
+
     @Test
     public void ensureNoDuplicates() {
-        ViolationsReportAsCommentBuilder builder = new ViolationsReportAsCommentBuilder();
+
+        // Prepare mock data
         final int nbIssue = 3;
+        final String[] checknames = { AssignedButStillOnSET.class.getSimpleName(), PostMissingPmAck.class.getSimpleName()};
         Map<Issue, List<Violation>> violations = new HashMap<Issue, List<Violation>>(nbIssue);
         List<Issue> issues = MockUtils.generateMockIssues(nbIssue, "JBEAP-10", "Issue nb");
         for ( Issue issue : issues )
-            violations.put(issue,MockUtils.generateMockViolationsForIssue(issue.getTrackerId().get(),
-                    AssignedButStillOnSET.class.getSimpleName(),
-                    PostMissingPmAck.class.getSimpleName()));
-        System.out.println(violations.size());
+            violations.put(issue,MockUtils.generateMockViolationsForIssue(issue.getTrackerId().get(), checknames));
+        // Run and asserts
         for (Map.Entry<Issue, Comment> entry: builder.reportViolationToBugTracker(violations).entrySet() ) {
-            System.out.println(entry.getKey().getTrackerId().get());
-            System.out.println(entry.getValue().getBody());
+            for ( String checkname : checknames ) {
+                final int nbOccurences = StringUtils.occurencesInString(StringUtils.formatCheckname(checkname), entry.getValue().getBody());
+                if (nbOccurences > 1)
+                    fail("Checkname " + checkname + " appears more than once in comment body:\n" + entry.getValue().getBody());
+            }
         }
     }
 
+    @Test
+    public void ensureNoNewCommentAdded() {
+        final String checkname = AssignedButStillOnSET.class.getSimpleName();
+        final String issueId = "JBEAP-666";
+        Comment comment = MockUtils.mockComment("10", StringUtils.formatCheckname(checkname), issueId);
+        Violation violation = MockUtils.mockViolation(issueId,checkname);
+        List<Violation> violationsToReport = new ArrayList<Violation>(0);
+        builder.addViolationToReportIfNotAlreadyReported(checkname,Arrays.asList(comment),violationsToReport,violation);
+        assertTrue(violationsToReport.isEmpty());
+    }
 
+
+    @Test
+    public void ensureNewCommentAddedIfChecknameDiffers() {
+        final String checkname = AssignedButStillOnSET.class.getSimpleName();
+        final String issueId = "JBEAP-666";
+        Comment comment = MockUtils.mockComment("10", StringUtils.formatCheckname(PostMissingPR.class.getSimpleName()), issueId);
+        Violation violation = MockUtils.mockViolation(issueId,checkname);
+        List<Violation> violationsToReport = new ArrayList<Violation>(0);
+        builder.addViolationToReportIfNotAlreadyReported(checkname,Arrays.asList(comment),violationsToReport,violation);
+        assertTrue(violationsToReport.size() == 1);
+    }
 }
