0a3c47832ec5d6f573cd2f7880f1a677977a8a00
Romain Pelisse <belaran@gmail.com>
2016-05-06 10:46:28 +0200

    Add a commidity method asList to CollectionUtils

--- src/test/java/org/jboss/jbossset/bugclerk/checks/utils/CollectionUtils.java
+++ src/test/java/org/jboss/jbossset/bugclerk/checks/utils/CollectionUtils.java
@@ -1,6 +1,8 @@
 package org.jboss.jbossset.bugclerk.checks.utils;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public final class CollectionUtils {
@@ -14,5 +16,14 @@ public final class CollectionUtils {
         for (T item : items)
             releasesSet.add(item);
         return releasesSet;
+    }
+
+    @SafeVarargs
+    public static <T> List<T> asListOf(T... items) {
+        List<T> list = new ArrayList<T>(items.length);
+        for ( T item : items) {
+            list.add(item);
+        }
+        return list;
     }
 }
