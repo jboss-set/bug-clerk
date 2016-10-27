1c32137f5b3fa39168ab2cc0db88fdeac2e6ec94
Romain Pelisse <belaran@gmail.com>
2016-10-27 10:38:44 +0200

    Fix run-on-bugid script: - add missing parameters for tracker creds - checks that BUGCLERK_HOME and BUGCLERK_VERSION have been defined

--- src/main/bash/run-on-bugid.sh
+++ src/main/bash/run-on-bugid.sh
@@ -6,11 +6,39 @@ readonly BUGCLERK_HOME=${BUGCLERK_HOME:-'target'}
 readonly MAIN_CLASS=${MAIN_CLASS:-'org.jboss.jbossset.bugclerk.cli.BugClerkCLI'}
 
 readonly BUG_ID=${1}
+
 shift
+
+if [ -z "${TRACKER_USERNAME}" ]; then
+  echo "Missing Bugzilla Username"
+  exit 1
+fi
+
+if [ -z "${TRACKER_PASSWORD}" ]; then
+  echo "Missing Bugzilla Password"
+  exit 2
+fi
+
+if [ ! -d "${BUGCLERK_HOME}" ]; then
+  echo "Invalid BUGCLERK_HOME (not a directory): ${BUGCLERK_HOME}"
+  exit 3
+fi
+
+if [ -z "${BUGCLERK_VERSION}" ]; then
+  echo "BUGCLERK_VERSION has not been defined !${BUGCLERK_VERSION}"
+  exit 4
+fi
+
+if [ ! -e "${BUGCLERK_HOME}/${JAR_NAME}-${BUGCLERK_VERSION}.jar" ]; then
+  echo "No jar file in directory: ${BUGCLERK_HOME}."
+  exit 4
+fi
 
 if [ -z "${BUG_ID}" ]; then
   echo "No bug ID provided."
   exit 1
 fi
 
-java -cp "${BUGCLERK_HOME}/bugclerk-${BUGCLERK_VERSION}.jar" "${MAIN_CLASS}" "${BUG_ID}" ${@}
+java -cp "${BUGCLERK_HOME}/bugclerk-${BUGCLERK_VERSION}.jar" "${MAIN_CLASS}" \
+     -u "${TRACKER_USERNAME}" -p "${TRACKER_PASSWORD}"
+      "${BUG_ID}" ${@}
