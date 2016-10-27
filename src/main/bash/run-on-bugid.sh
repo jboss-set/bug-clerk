063d91ebf01b80c4133023f4a376001e03e976c1
Romain Pelisse <belaran@gmail.com>
2016-10-27 10:34:43 +0200

    removed unused var in run-on-bugid.sh

--- src/main/bash/run-on-bugid.sh
+++ src/main/bash/run-on-bugid.sh
@@ -4,7 +4,6 @@ readonly BUGCLERK_VERSION=${BUGCLERK_VERSION:-'0.7.0-SNAPSHOT'}
 readonly BUGCLERK_HOME=${BUGCLERK_HOME:-'target'}
 
 readonly MAIN_CLASS=${MAIN_CLASS:-'org.jboss.jbossset.bugclerk.cli.BugClerkCLI'}
-readonly URL_PREFIX=${URL_PREFIX:-'https://bugzilla.redhat.com/show_bug.cgi?id='}
 
 readonly BUG_ID=${1}
 shift
