ebd294949560f3d9a8d5347b09fffe2fb1cbbd5d
Romain Pelisse <belaran@gmail.com>
2016-10-28 13:48:47 +0200

    Allows to override FILTER_ID in filter-based-run

--- src/main/bash/filter-based-run.sh
+++ src/main/bash/filter-based-run.sh
@@ -10,9 +10,9 @@ readonly JAR_NAME=${JAR_NAME:-'bugclerk'}
 
 readonly BZ_SERVER_URL='https://bugzilla.redhat.com'
 
-readonly EAP7_UNRESOLVED='12328451'
+readonly FILTER_ID=${FILTER_ID:-'12328451'}
 readonly FILTER_URL_ENDPOINT='https://issues.jboss.org/rest/api/latest/filter'
-readonly FILTER_URL=${FILTER_URL:-${FILTER_URL_ENDPOINT}/${EAP7_UNRESOLVED}}
+readonly FILTER_URL=${FILTER_URL:-${FILTER_URL_ENDPOINT}/${FILTER_ID}}
 
 usage() {
   echo "$(basename ${0})"
