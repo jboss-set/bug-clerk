10c30cc171da38822ad6a5378ae696efebf361ce
Pelisse Romain <belaran@redhat.com>
2017-07-12 11:47:06 +0200

    Update MissingDownstreamDependencyLabel.java

--- src/test/java/org/jboss/jbossset/bugclerk/checks/MissingDownstreamDependencyLabel.java
+++ src/test/java/org/jboss/jbossset/bugclerk/checks/MissingDownstreamDependencyLabel.java
@@ -135,5 +135,4 @@ public class MissingDownstreamDependencyLabel extends AbstractCheckRunner {
         assertResultsIsAsExpected(engine.runCheckOnBugs(CollectionUtils.asSetOf(new Candidate(issue)), checkName),
                 checkName, bugId, 1);
     }
-
 }
