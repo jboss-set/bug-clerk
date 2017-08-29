e3b2492cd846daf6393a59d807abb32bd5c556a4
Romain Pelisse <belaran@gmail.com>
2017-08-29 16:36:20 +0200

    misc: clean script

--- dist/src/main/bash/filter-based-run.sh
+++ dist/src/main/bash/filter-based-run.sh
@@ -4,9 +4,7 @@ readonly APHRODITE_CONFIG=${APHRODITE_CONFIG:-"$(pwd)/aphrodite-config.json"}
 
 readonly BUGCLERK_HOME=${BUGCLERK_HOME:-'.'}
 readonly BUGCLERK_VERSION=${BUGCLERK_VERSION:-${project.version}}
-readonly JAR_NAME=${JAR_NAME:-'bugclerk'}
-
-readonly BZ_SERVER_URL='https://bugzilla.redhat.com'
+readonly JAR_NAME=${JAR_NAME:-'bugclerk-dist'}
 
 readonly FILTER_ID=${FILTER_ID:-'12330472'}
 readonly FILTER_URL_ENDPOINT='https://issues.jboss.org/rest/api/latest/filter'
@@ -19,23 +17,23 @@ usage() {
 
 if [ ! -d "${BUGCLERK_HOME}" ]; then
   echo "Invalid BUGCLERK_HOME (not a directory): ${BUGCLERK_HOME}"
-  exit 3
+  exit 1
 fi
 
 if [ -z "${BUGCLERK_VERSION}" ]; then
   echo "BUGCLERK_VERSION has not been defined !${BUGCLERK_VERSION}"
-  exit 4
+  exit 2
 fi
 
 readonly FULL_PATH_TO_JAR="${BUGCLERK_HOME}/${JAR_NAME}-${BUGCLERK_VERSION}-shaded.jar"
 if [ ! -e "${FULL_PATH_TO_JAR}" ]; then
   echo "No jar file in directory: ${FULL_PATH_TO_JAR}."
-  exit 4
+  exit 3
 fi
 
 cd "${BUGCLERK_HOME}"
 java -Daphrodite.config="${APHRODITE_CONFIG}" \
-     -jar ./bugclerk-${BUGCLERK_VERSION}-shaded.jar \
+     -jar ${FULL_PATH_TO_JAR} \
      -f "${FILTER_URL}" \
      ${@}
 readonly STATUS=${?}
