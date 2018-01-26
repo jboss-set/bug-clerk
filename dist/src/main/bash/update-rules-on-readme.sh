6b750f0caa31b94750b7154f7aa522a95fb8dba5
Romain Pelisse <belaran@gmail.com>
2018-01-26 12:13:07 +0100

    Update update-rules script broken since refactoring introduced core and dist module

--- dist/src/main/bash/update-rules-on-readme.sh
+++ dist/src/main/bash/update-rules-on-readme.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-readonly RULE_URL_PREFIX='https://github.com/jboss-set/bug-clerk/tree/master/src/main/resources/org/jboss/jbossset/bugclerk'
 
+readonly RULES_RELATIVE_FOLDER_NAME='core/src/main/resources/org/jboss/jbossset/bugclerk/'
+readonly RULE_URL_PREFIX="https://github.com/jboss-set/bug-clerk/tree/master/${RULES_RELATIVE_FOLDER_NAME}"
 
-grep -e 'rule' src/main/resources/org/jboss/jbossset/bugclerk/*.drl | sed -e '/FilterIssueEntries.drl/d' | cut -f2 -d: | \
+grep -e 'rule' ${RULES_RELATIVE_FOLDER_NAME}/*.drl | sed -e '/FilterIssueEntries.drl/d' | cut -f2 -d: | \
     sed -e 's/"//g' -e 's/rule //' -e 's/_.*$//' | sort -u | \
 while
   read rulename
