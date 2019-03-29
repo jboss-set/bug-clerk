fc385c1a8a31fa0c2432576bf86feeb15a830b51
Chao Wang <chaowan@redhat.com>
2019-03-29 16:26:08 +0800

    Log a warning message instead of throw exception.

--- core/src/main/java/org/jboss/jbossset/bugclerk/aphrodite/callables/GetPullRequest.java
+++ core/src/main/java/org/jboss/jbossset/bugclerk/aphrodite/callables/GetPullRequest.java
@@ -1,7 +1,9 @@
 package org.jboss.jbossset.bugclerk.aphrodite.callables;
 
 import java.net.URL;
+import java.util.logging.Level;
 
+import org.jboss.jbossset.bugclerk.utils.LoggingUtils;
 import org.jboss.set.aphrodite.Aphrodite;
 import org.jboss.set.aphrodite.domain.PullRequest;
 import org.jboss.set.aphrodite.spi.NotFoundException;
@@ -20,8 +22,8 @@ public class GetPullRequest extends AphroditeCallable<PullRequest> {
         try {
             return aphrodite.getPullRequest(pullRequest);
         } catch (NotFoundException e) {
-            throw new IllegalArgumentException("No such Pull Requests:" + pullRequest, e);
+            LoggingUtils.getLogger().log(Level.WARNING, "Unable to get pull request from url : " + pullRequest);
+            return null;
         }
     }
-
 }
